b958e6df78a480dd2147a2ecc60943680417e51e
AnaPereira38 <202002038@estudantes.ips.pt>
2024-02-14 17:34:19 +0000

    ExpandirItem E SelecionarEstado

--- CSSC/CSSC/wwwroot/js/site.js
+++ CSSC/CSSC/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener('DOMContentLoaded', function () {
+    var toggleButtons = document.querySelectorAll('.toggle-details-btn');
+
+    toggleButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            var row = this.closest('.expandable-row');
+            var hiddenRow = row.nextElementSibling;
+            hiddenRow.classList.toggle('show-details');
+        });
+    });
+});
