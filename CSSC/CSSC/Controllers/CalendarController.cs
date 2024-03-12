5ebcbf7db9e8b56737ef398ed1ed345a84426f4f
jorgeOliveira1510 <jorge.oliveira15101999@gmail.com>
2024-03-12 19:10:30 +0000

    Validações das datas e filtragem dos serviços no calendário

--- CSSC/CSSC/Controllers/CalendarController.cs
+++ CSSC/CSSC/Controllers/CalendarController.cs
@@ -7,6 +7,7 @@ using CSSC.Areas.Identity.Data;
 using CSSC.Models;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
+using CSSC.Controllers;
 
 public class CalendarController : Controller
 {
@@ -69,48 +70,132 @@ public class CalendarController : Controller
     /// <param name="endDate">A data de término do calendário.</param>
     /// <returns>Um ActionResult representando a vista do calendário.</returns>
     [HttpPost]
-    public async Task<ActionResult> Index(DateTime startDate, DateTime endDate)
+    public async Task<ActionResult> Index(DateTime startDate, DateTime endDate, bool agendadoCheckbox, bool concluidoCheckbox)
     {
-        var months = GetMonthsBetween(startDate, endDate);
-
-        if (User.IsInRole("Operador"))
+        if (startDate == DateTime.MinValue || endDate == DateTime.MinValue)
         {
-            var services = _context.ServiceModel
-                .Include(r => r.csscUser)
-                .ToList();
-            
-            var data = new List<List<Services>>();
-            foreach (var month in months)
-            {
-                var monthData = services.Where(s => s.ServPrazo.Month == DateTime.ParseExact(month, "MMMM yyyy", CultureInfo.CreateSpecificCulture("pt-PT")).Month
-                                                    && s.ServPrazo.Year == DateTime.ParseExact(month, "MMMM yyyy", CultureInfo.CreateSpecificCulture("pt-PT")).Year).ToList();
-                data.Add(monthData);
-            }
-
-            var viewModel = new CalendarViewModel() { Months = months, Data = data };
-            return View(viewModel);
-        }
-        else if (User.IsInRole("Default"))
-        {
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var userServices = await _context.ServiceModel
-                .Include(r => r.csscUser)
-                .Where(r => r.ServIdUtilizador == Guid.Parse(userId)).ToListAsync();
-            
-            var data = new List<List<Services>>();
-            foreach (var month in months)
-            {
-                var monthData = userServices.Where(s => s.ServPrazo.Month == DateTime.ParseExact(month, "MMMM", CultureInfo.CreateSpecificCulture("pt-PT")).Month
-                                                    && s.ServPrazo.Year == DateTime.ParseExact(month, "MMMM", CultureInfo.CreateSpecificCulture("pt-PT")).Year).ToList();
-                data.Add(monthData);
-            }
-
-            var viewModel = new CalendarViewModel() { Months = months, Data = data };
-            return View(viewModel);
+            TempData["IncompleteDates"] = true;
+            return RedirectToAction("Index", "Calendar");
         }
         else
         {
-            return RedirectToPage("/Account/Login", new { area = "Identity" });
+            var months = GetMonthsBetween(startDate, endDate);
+            var data = new List<List<Services>>();
+
+            if (User.IsInRole("Operador"))
+            {
+                if (agendadoCheckbox == true && concluidoCheckbox == false)
+                {
+                    var services = _context.ServiceModel
+                        .Include(r => r.csscUser)
+                        .Where(s => s.EstadoDoServico == EstadoDoServico.Espera.ToString())
+                        .ToList();
+                    foreach (var month in months)
+                    {
+                        var monthData = services.Where(s =>
+                            s.ServPrazo.Month == DateTime.ParseExact(month, "MMMM yyyy",
+                                CultureInfo.CreateSpecificCulture("pt-PT")).Month
+                            && s.ServPrazo.Year == DateTime.ParseExact(month, "MMMM yyyy",
+                                CultureInfo.CreateSpecificCulture("pt-PT")).Year).ToList();
+                        data.Add(monthData);
+                    }
+                }
+                else if (concluidoCheckbox == true && agendadoCheckbox == false)
+                {
+                    var services = _context.ServiceModel
+                        .Include(r => r.csscUser)
+                        .Where(s => s.EstadoDoServico == EstadoDoServico.Entregue.ToString())
+                        .ToList();
+                    foreach (var month in months)
+                    {
+                        var monthData = services.Where(s =>
+                            s.ServPrazo.Month == DateTime.ParseExact(month, "MMMM yyyy",
+                                CultureInfo.CreateSpecificCulture("pt-PT")).Month
+                            && s.ServPrazo.Year == DateTime.ParseExact(month, "MMMM yyyy",
+                                CultureInfo.CreateSpecificCulture("pt-PT")).Year).ToList();
+                        data.Add(monthData);
+                    }
+                }
+                else
+                {
+                    var services = _context.ServiceModel
+                        .Include(r => r.csscUser)
+                        .ToList();
+                    foreach (var month in months)
+                    {
+                        var monthData = services.Where(s =>
+                            s.ServPrazo.Month == DateTime.ParseExact(month, "MMMM yyyy",
+                                CultureInfo.CreateSpecificCulture("pt-PT")).Month
+                            && s.ServPrazo.Year == DateTime.ParseExact(month, "MMMM yyyy",
+                                CultureInfo.CreateSpecificCulture("pt-PT")).Year).ToList();
+                        data.Add(monthData);
+                    }
+                }
+
+                var viewModel = new CalendarViewModel() { Months = months, Data = data };
+                return View(viewModel);
+            }
+            else if (User.IsInRole("Default"))
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                if (agendadoCheckbox == true && concluidoCheckbox == false)
+                {
+                    var userServices = await _context.ServiceModel
+                        .Include(r => r.csscUser)
+                        .Where(r => r.ServIdUtilizador == Guid.Parse(userId))
+                        .Where(s => s.EstadoDoServico == EstadoDoServico.Espera.ToString()).ToListAsync();
+
+                    foreach (var month in months)
+                    {
+                        var monthData = userServices.Where(s =>
+                            s.ServPrazo.Month == DateTime
+                                .ParseExact(month, "MMMM", CultureInfo.CreateSpecificCulture("pt-PT")).Month
+                            && s.ServPrazo.Year == DateTime
+                                .ParseExact(month, "MMMM", CultureInfo.CreateSpecificCulture("pt-PT")).Year).ToList();
+                        data.Add(monthData);
+                    }
+
+                }else if (concluidoCheckbox == true && agendadoCheckbox == false)
+                {
+                    var userServices = await _context.ServiceModel
+                        .Include(r => r.csscUser)
+                        .Where(r => r.ServIdUtilizador == Guid.Parse(userId))
+                        .Where(s => s.EstadoDoServico == EstadoDoServico.Entregue.ToString()).ToListAsync();
+
+                    foreach (var month in months)
+                    {
+                        var monthData = userServices.Where(s =>
+                            s.ServPrazo.Month == DateTime
+                                .ParseExact(month, "MMMM", CultureInfo.CreateSpecificCulture("pt-PT")).Month
+                            && s.ServPrazo.Year == DateTime
+                                .ParseExact(month, "MMMM", CultureInfo.CreateSpecificCulture("pt-PT")).Year).ToList();
+                        data.Add(monthData);
+                    }
+                }
+                else
+                {
+                    var userServices = await _context.ServiceModel
+                        .Include(r => r.csscUser)
+                        .Where(r => r.ServIdUtilizador == Guid.Parse(userId)).ToListAsync();
+
+                    foreach (var month in months)
+                    {
+                        var monthData = userServices.Where(s =>
+                            s.ServPrazo.Month == DateTime
+                                .ParseExact(month, "MMMM", CultureInfo.CreateSpecificCulture("pt-PT")).Month
+                            && s.ServPrazo.Year == DateTime
+                                .ParseExact(month, "MMMM", CultureInfo.CreateSpecificCulture("pt-PT")).Year).ToList();
+                        data.Add(monthData);
+                    }
+                }
+
+                var viewModel = new CalendarViewModel() { Months = months, Data = data };
+                return View(viewModel);
+            }
+            else
+            {
+                return RedirectToPage("/Account/Login", new { area = "Identity" });
+            }
         }
     }
 
